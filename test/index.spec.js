71b26bfb35418af5564e1c093bb9ce30758a1fa5
Lukas Siemon <lukas.siemon@gmail.com>
2019-07-31 19:19:40 -0700

    feat: added rule to ensure e.g. typeof ... === 'object' is not used

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,7 @@ it('Test Exported Rules', () => {
   expect(Object.keys(index.rules).sort()).to.deep.equal([
     'istanbul-prevent-ignore',
     'jasmine-expect-two-arguments',
-    'kebab-case-enforce'
+    'kebab-case-enforce',
+    'prevent-typeof-object'
   ]);
 });
