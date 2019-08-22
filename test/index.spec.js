eb9e08a3f3b3d9edab79920252c955f88ed5eb07
Lukas Siemon <github@blackflux.com>
2019-08-22 14:43:17 -0700

    fix: removed unnecessary rules

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,9 +3,7 @@ const index = require('../src/index');
 
 it('Test Exported Rules', () => {
   expect(Object.keys(index.rules).sort()).to.deep.equal([
-    'enforce-optimal-require',
     'istanbul-prevent-ignore',
-    'jasmine-expect-two-arguments',
     'kebab-case-enforce',
     'prevent-typeof-object'
   ]);
