9e1766824ffea4ed3992946da4ab4605461ea53d
Lukas Siemon <lukas.siemon@blackflux.com>
2022-02-09 16:17:57 -0800

    feat: added c8-prevent-ignore

---
+++ src/rules/c8-prevent-ignore.js
@@ -0,0 +1,17 @@
+module.exports = {
+  create: (context) => {
+    const sourceCode = context.getSourceCode();
+
+    const testComment = (node) => {
+      if (node.value.startsWith(' c8 ignore ')) {
+        context.report({ node, message: 'Not allowed to ignore coverage in this file.' });
+      }
+    };
+
+    return {
+      Program() {
+        sourceCode.getAllComments().filter((token) => token.type !== 'Shebang').forEach(testComment);
+      }
+    };
+  }
+};
